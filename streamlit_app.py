bac41fc5e8d4a696425a1eb13ef97fb12279334c
ryan <ryanbroguiere123@gmail.com>
2023-02-01 01:23:29 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,6 +92,5 @@ if streamlit.button('Add a Fruit to the List'):
   back_from_function = insert_row_snowflake(add_my_fruit)
   streamlit.text(back_from_function)
 
-streamlit.write('Thanks for adding', add_my_fruit)
 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
